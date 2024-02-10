716fbe458d8adf15514fb18f685a4a803a1a282e
ZaidievArtur <zajdiev.ai@edu.spbstu.ru>
2024-02-10 21:14:00 +0300

    V1.0.0
    Almost final product

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -12,11 +12,25 @@ double discriminant(double a,double b,double c){
     return d;
 }
 
+void solution(double a,double b,double c){
+    double d = discriminant(a,b,c);
+    if (d < 0) std::cout<<"No roots"<<std::endl;
+    if (d == 0){
+        double x = -b/(2*a);
+        std::cout<<"Only one root: x = "<<x<<std::endl;
+    }
+    if (d >0) {
+        double x1 = (-b-sqrt(d))/(2*a);
+        double x2 = (-b+sqrt(d))/(2*a);
+        std::cout<<"Your roots: x1 = "<<x1<<" and x2 = "<<x2<<std::endl;
+    }
+
+}
+
 int main()
 {
     double a,b,c;
     input(a,b,c);
 
-    double d = discriminant(a, b, c);
-    std::cout<<"Your dis = "<<d;
+    solution(a,b,c);
 }
