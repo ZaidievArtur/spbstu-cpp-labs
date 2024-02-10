34228af61a553184611000e894f8c45bca8b7cdf
ZaidievArtur <zajdiev.ai@edu.spbstu.ru>
2024-02-10 21:09:34 +0300

    function for discriminant

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -6,9 +6,17 @@ double input (double &a, double &b, double &c){
     std::cin >>a >> b >> c;
 }
 
+double discriminant(double a,double b,double c){
+    double d;
+    d = (b*b) - (4 * a * c);
+    return d;
+}
+
 int main()
 {
     double a,b,c;
     input(a,b,c);
-    std::cout<<a<<b<<c;
+
+    double d = discriminant(a, b, c);
+    std::cout<<"Your dis = "<<d;
 }
